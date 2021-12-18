64d1700085ef694f11e31bc8d6c32910376b70ad
girvel <widauka@ya.ru>
2021-12-18 17:36:01 +0500

    #

--- tests/test_ecs.py
+++ tests/test_ecs.py
@@ -2,6 +2,19 @@ import pytest
 import ecs
 
 
+@pytest.fixture
+def pairs_system():
+  class PairsSystem:
+    class Members:
+      first = ["name"]
+      second = ["name"]
+      list_container = ["pairs"]
+    
+    @staticmethod
+    def process(first, second, list_container):
+        
+
+
 def test_entity_creation():
   entity = ecs.Entity(
     name='custom-entity',
