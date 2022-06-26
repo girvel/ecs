8d560823b8fe77ea18e1372e82146da5c91cdf8f
girvel <widauka@ya.ru>
2022-06-26 22:45:24 +0400

    Fix __repr__ bugs

--- ecs/core.py
+++ ecs/core.py
@@ -1,3 +1,23 @@
+def pretty(x, max_length=50):
+	if isinstance(x, Entity):
+		return ('name' in x
+			and short(x.name)
+			or f"<Entity l={len(x)}>"
+		)
+
+	if any(isinstance(x, primitive) for primitive in [int, float, str]):
+		return short(repr(x))
+
+	return f"<{short(type(x).__name__)}>"
+
+
+def short(string, max_length=50):
+	string = str(string)
+	if len(string) > max_length:
+		string = string[:max_length - 3] + "..."
+	return string
+
+
 class Entity:
 	"""Entity is a mixture of dict and object.
 
@@ -44,12 +64,6 @@ class Entity:
 
 	def __repr__(self):
 		name = self["name", None]
-
-		def pretty(x, max_length=50):
-			string = repr(x)
-			if len(string) > max_length:
-				string = string[:max_length - 3] + "..."
-			return string
 
 		return 'Entity{}({})'.format(
 			name and f" '{name}'" or "",
