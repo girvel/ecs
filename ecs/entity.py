961ef87c1434a084e6b0e3f128b6b1aa689e1d95
girvel <widauka@ya.ru>
2023-09-03 17:51:03 +0500

    Failed attempt in benchmarking via Jupyter

--- ecs/entity.py
+++ ecs/entity.py
@@ -7,46 +7,19 @@ class Entity:
     You can access attributes as items.
     """
 
-    def __init__(self, **attributes):
+    def __init__(self, attributes):
         """
         Args:
             **attributes: entity's future attributes in format
         """
-        for k, v in attributes.items():
-            setattr(self, k, v)
-
-    def __delitem__(self, key):
-        """Deletes an attribute with the given name."""
-        return delattr(self, key)
-
-    def __getitem__(self, item):
-        """Gets an attribute with the given name.
-
-        Args:
-            item: a name of the attribute or a tuple (name, default_value)
-
-        Returns:
-            Attribute value or default value if specified
-        """
-        if isinstance(item, tuple):
-            return getattr(self, *item)
-
-        return getattr(self, item)
-
-    def __setitem__(self, item, value):
-        """Sets an attribute with the given name."""
-        return setattr(self, item, value)
-
-    def __contains__(self, item):
-        """Checks if entity contains an attribute with the given name."""
-        return hasattr(self, item)
+        self._attributes = attributes
 
     def __len__(self):
         """Counts number of attributes inside the entity."""
-        return len(list(self.__iter__()))
+        return len(self._attributes)
 
     def __repr__(self):
-        name = self["name", None]
+        name = self._attributes.get("name", None)
 
         return 'Entity{}({})'.format(
             name and f" '{name}'" or "",
@@ -59,7 +32,36 @@ class Entity:
 
     def __iter__(self):
         """Iterates entity as pairs: (attribute_name, attribute_value)"""
+        yield from self._attributes.items()
 
-        for attr_name in dir(self):
-            if not attr_name.startswith('__') or not attr_name.endswith('__'):
-                yield attr_name, getattr(self, attr_name)
+    def __contains__(self, item):
+        """Checks if entity contains an attribute with the given name."""
+        return item in self._attributes
+
+    def __delitem__(self, key):
+        """Deletes an attribute with the given name."""
+        del self._attributes[key]
+
+    def __getitem__(self, item):
+        """Gets an attribute with the given name.
+
+        Args:
+            item: a name of the attribute or a tuple (name, default_value)
+
+        Returns:
+            Attribute value or default value if specified
+        """
+        return self._attributes[item]
+
+    def __setitem__(self, item, value):
+        """Sets an attribute with the given name."""
+        self._attributes[item] = value
+
+    def __delattr__(self, item):
+        del self._attributes[item]
+
+    def __getattr__(self, item):
+        return self._attributes[item]
+
+    def __setattr__(self, item, value):
+        self._attributes[item] = value
