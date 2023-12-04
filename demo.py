2b126f728d4d476e5925dbeabf7fe80081a72e77
girvel <widauka@ya.ru>
2023-12-04 09:34:26 +0500

    Rewrite demo

--- demo.py
+++ demo.py
@@ -1,39 +1,61 @@
-from ecs import Metasystem, create_system  # TODO NEXT fix demo
+from ecs import MetasystemFacade, System, Entity  # TODO NEXT fix demo
 import time
 
 
 # 1. You create a metasystem
-ms = Metasystem()
+ms = MetasystemFacade()
 
 dt = 0.04
 
 # 2. You create systems and add them to metasystem
-@ms.add
-@create_system
-def gravity(
-    object: 'vy',
-    constants: 'g',
-):
-    object.vy += constants.g * dt
+class VerticalSpeed:
+    vy: float
+
+class GravityConstants:
+    g: float
 
 @ms.add
-@create_system
-def inertion(object: 'x, y, vx, vy'):
-    object.x += object.vx * dt
-    object.y += object.vy * dt
+@System
+def gravity(target: VerticalSpeed, constants: GravityConstants):
+    target.vy += constants.g * dt
+
+class Inert:
+    x: float
+    y: float
+    vx: float
+    vy: float
 
 @ms.add
-@create_system
-def output(object: 'name, x, y'):
+@System
+def inertia(target: Inert):
+    target.x += target.vx * dt
+    target.y += target.vy * dt
+
+class Displayable:
+    x: float
+    y: float
+    name: str
+
+@ms.add
+@System
+def output(target: Displayable):
     yield from range(int(1 / dt) - 1)  # skips 24 out of 25 frames
-    print(f'{object.name}: {object.x:.2f}, {object.y:.2f}')
+    print(f'{target.name}: {target.x:.2f}, {target.y:.2f}')
+
 
 # 3. You create objects
-ms.create(name='falling_guy1', x=0, y=0, vx=0, vy=0)
-ms.create(name='falling_guy2', x=100, y=0, vx=0, vy=0)
-ms.create(g=10)
+class DynamicEntity(Entity):
+    def __init__(self, **attributes):
+        for key, value in attributes.items():
+            setattr(self, key, value)
+
+ms.add(DynamicEntity(name='falling_guy1', x=0, y=0, vx=0, vy=0))
+ms.add(DynamicEntity(name='falling_guy2', x=100, y=0, vx=0, vy=0))
+ms.add(DynamicEntity(g=10))
+
 
 # 4. Game loop
-while True:
-    ms.update()
-    time.sleep(dt)
+if __name__ == "__main__":
+    while True:
+        ms.update()
+        time.sleep(dt)
